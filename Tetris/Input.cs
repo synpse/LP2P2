0888339404189e5fa8688ce77fd7d18e24213974
IssaMaia <Diogo_maia99@hotmail.com>
2018-12-27 00:29:21 +0000

    Added Input and Render Class

---
+++ Tetris/Input.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Diagnostics;
+using System.Media;
+using System.Resources;
+using System.IO;
+using System.Reflection;
+
+namespace Tetris
+{
+    class Input
+    {
+        public ConsoleKeyInfo key;
+
+        public void Readkey(bool isKeyPressed, Piece tet)
+        {
+            if (Console.KeyAvailable)
+            {
+                key = Console.ReadKey();
+                isKeyPressed = true;
+            }
+            else
+                isKeyPressed = false;
+
+            if (key.Key == ConsoleKey.LeftArrow & !tet.isSomethingLeft() & isKeyPressed)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    tet.Location[i][1] -= 1;
+                }
+                tet.PositionUpdate(Game.grid, Game.droppedtetrominoeLocationGrid);
+            }
+            else if (key.Key == ConsoleKey.RightArrow & !tet.isSomethingRight() & isKeyPressed)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    tet.Location[i][1] += 1;
+                }
+                tet.PositionUpdate(Game.grid, Game.droppedtetrominoeLocationGrid);
+            }
+            if (key.Key == ConsoleKey.DownArrow & isKeyPressed)
+            {
+                tet.Drop(Game.grid, Game.droppedtetrominoeLocationGrid);
+            }
+            if (key.Key == ConsoleKey.UpArrow & isKeyPressed)
+            {
+                for (; tet.isSomethingBelow() != true;)
+                {
+                    tet.Drop(Game.grid, Game.droppedtetrominoeLocationGrid);
+                }
+            }
+            if (key.Key == ConsoleKey.Spacebar & isKeyPressed)
+            {
+                //rotate
+                tet.Rotate();
+                tet.PositionUpdate(Game.grid, Game.droppedtetrominoeLocationGrid);
+            }
+        }
+    }
+}
---
+++ Tetris/Render.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Render
+    {
+        public void Draw(int[,] grid, int[,] droppedtetrominoeLocationGrid)
+        {
+            for (int i = 0; i < 23; ++i)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Square square = new Square();
+
+                    Console.SetCursorPosition(1 + 2 * j, i);
+                    if (grid[i, j] == 1 | droppedtetrominoeLocationGrid[i, j] == 1)
+                    {
+                        Console.SetCursorPosition(1 + 2 * j, i);
+                        Console.ForegroundColor = Tetromino.CurrentColor;
+                        Console.Write(square.Sqr);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+            }
+        }
+
+        public void DrawBorder()
+        {
+            for (int lengthCount = 0; lengthCount <= 22; ++lengthCount)
+            {
+                Console.SetCursorPosition(0, lengthCount);
+                Console.Write("|");
+                Console.SetCursorPosition(21, lengthCount);
+                Console.Write("|");
+            }
+            Console.SetCursorPosition(0, 23);
+            for (int widthCount = 0; widthCount <= 10; widthCount++)
+            {
+                Console.Write("■■");
+            }
+        }
+    }
+}
